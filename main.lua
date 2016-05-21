996a69d13efdb2ce22bdfef838a6665a52ae9aed
rrwiyatn <rey.reza@hotmail.com>
2016-05-20 20:46:33 -0700

    update main.lua

--- main.lua
+++ main.lua
@@ -12,26 +12,22 @@ srv:listen(80,function(conn)
         end
         local duty=light_level*1023/100
         pwm.setup(3, 500, duty)
-        local message={}
+        local message = {}
         print("Level:"..light_level)
         if(method == "POST")then --light_level was sent from node.js as the header of the request
            if(duty>0)then
               pwm.start(3)
               print("LED ON, POWER:"..light_level)
-              message[#message + 1] = "HTTP/1.1 200 OK\r\n"
-              message[#message + 1] = "Content-Type: text/html\r\n\r\n"
-              message[#message + 1] = "POST request successfully received\r\n"
+              message[#message + 1] = {"HTTP/1.0 200 OK\r\n Content-Type: text/html\r\n\r\n"}
+              message[#message + 1] = (light_level/100)
            elseif(duty==0)then
               pwm.stop(3)
               print("LED OFF")
-              message[#message + 1] = "HTTP/1.1 200 OK\r\n"
-              message[#message + 1] = "Content-Type: text/html\r\n\r\n"
-              message[#message + 1] = "POST request successfully received\r\n"
+              message[#message + 1] = 0
            end 
         elseif(method == "GET")then
-           message[#message + 1] = "HTTP/1.1 200 OK\r\n"
-           message[#message + 1] = "Content-Type: text/html\r\n\r\n"
-           message[#message + 1] = "LED STATE="..tostring(pinState).."\r\n"
+           message[#message + 1] = {"HTTP/1.0 200 OK\r\n Content-Type: text/html\r\n\r\n"}
+           --message[#message + 1] = "LED STATE="..tostring(pinState).."\r\n"
         end
         local function send()
           if #message > 0 then 
@@ -49,7 +45,7 @@ srv:listen(80,function(conn)
            node.restart()
         end
         collectgarbage()
-        print("Memory Used:"..collectgarbage("count"))
-        print("Heap Available:"..heapSize)
+        --print("Memory Used:"..collectgarbage("count"))
+        --print("Heap Available:"..heapSize)
     end)
 end)
